9d272dedb1e75245ac1c0d3bf4615aefb89303b1
bedolpab <bedol.pab@gmail.com>
2024-07-21 03:27:41 -0700

    nvim settings

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,16 @@
+-- Default indents
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Line settings
+vim.opt.number = false
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 10
+
+-- More
+vim.opt.wrap = false
+vim.opt.mouse = "a"
+vim.opt.showmode = true
+vim.opt.cursorline = true
